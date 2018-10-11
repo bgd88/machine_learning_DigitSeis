50bfeefadb7b4b9b1ada9304ec9c1953998e3414
Miaki Ishii <miaki@menou.local>
2018-10-11 19:17:53 -0400

    modified extractImg.m so that .mat files within a given directory can all be processed automatically (or just a single .mat file depending upon ldir variable value)

--- extractImg.m
+++ extractImg.m
@@ -5,45 +5,58 @@
 %  and classified objects can be studied with artificial intelligence.
 
 %  October 06, 2018
-%  Last Modified: October 09, 2018
+%  Last Modified: October 11, 2018
 
-%% extract option parameters
+%% initial/extract option parameters
 
 lnoise = true;      % true if binary image includes noise objects 
                     % note that some analyses may have deleted all noise
                     % objects
-                    
-%% read file
 
-[filename cdir] = uigetfile('*.mat');
+ldir = true;        % true if processing all .mat files in a given directory
 
-if isfloat(filename)  % file not chosen
-    return
-end   % if isfloat(filename)
+%% get filenames to be read in and output directory name
 
-%% load needed information
+cfileList = {};
+cfilenames = {};
 
-clear Iorig S  % make sure needed files do not exist in the work space
-Iorig = []; S = [];  % empty variables so that we can use them to check
+if ldir  % get directory name and then all the .mat filenames within the directory
+    disp('Choose directory where all .mat files to be processed can be found');
+    cdir = uigetdir('.','Directory for all .mat files to be read in');
+    
+    if isfloat(cdir)  % input directory has not been chosen
+        return  % end program
+    end  % if isfloat(cdir)
+    
+    % get names of all .mat files within the directory
+    cfileList = dir(cdir);
+    if isempty(cfileList)  % if something went wrong
+        return  % end program
+    end  % if isempty(cfileList)
+    
+    % check to make sure there are a finite number of .mat files
+    cfilenames = {cfileList.name};
+    indx = find(~cellfun('isempty',regexp(cfilenames,'.mat\>')));
+    if isempty(indx)  % no .mat files
+        return  % end program
+    end  % if isempty(indx)
+    
+    cfilenames = cfilenames(indx);  % only keep .mat filenames
+else  % get a single .mat filename
+    [filename cdir] = uigetfile('*.mat');
+    
+    if isfloat(filename)  % file not chosen
+        return
+    end   % if isfloat(filename)
+    
+    cfilenames{1} = filename;
+end  % if ldir
 
-load([cdir filename],'Iorig','S');
+if ~strcmpi(cdir(end),'/')  % make sure cdir ends with '/'
+    cdir = [cdir '/'];
+end  % if ~strcmpi(cdir(end),'/')
 
-if isempty(Iorig) || isempty(S)
-    disp(['Iorig and/or S variables do not exist in ',[cdir filename]])
-    return
-end   % if isempty(Iorig) || isempty(S)
-
-Ibin = false(size(Iorig));
-if lnoise  % if including noise objects (i.e., all objects)
-    indx = 1:numel(S);
-else  % if ignoring noise objects
-    indx = find([S.ID]>=0);
-end  % if lnoise
-rpix = cell2mat({S(indx).PixelIdxList}');
-Ibin(rpix) = true;
-
-%% save information
-
+% save location information
 disp('Choose directory for saving results');
 cdirOut = uigetdir('.','Directory for Data to be Saved');
 
@@ -51,5 +64,37 @@ if isfloat(cdirOut)  % output directory has not been chosen
     cdirOut = '';  % empty directory name, i.e., save into current working directory
 end  % if isfloat(cdirOut)
 
-cfile = [filename(1:end-4) '.AI.mat'];
-save([cdirOut '/' cfile],'Iorig','Ibin','S');
+
+%% read and write information into files
+
+for k = 1:numel(cfilenames)
+    
+    % clear variables
+    clear Iorig Ibin S  % make sure needed files do not exist in the work space
+    Iorig = []; Ibin = []; S = [];  % empty variables so that we can use them to check
+    
+    % load data
+    filename = cfilenames{k};
+    disp(['working on file: ', filename]);
+    
+    load([cdir filename],'Iorig','S');
+    
+    if isempty(Iorig) || isempty(S)
+        disp(['Iorig and/or S variables do not exist in ',[cdir filename]])
+    else  % Iorig and S exist - get the binary image and save
+        Ibin = false(size(Iorig));
+        if lnoise  % if including noise objects (i.e., all objects)
+            indx = 1:numel(S);
+        else  % if ignoring noise objects
+            indx = find([S.ID]>=0);
+        end  % if lnoise
+        rpix = cell2mat({S(indx).PixelIdxList}');
+        Ibin(rpix) = true;
+        
+        % save information
+        cfile = [filename(1:end-4) '.AI.mat'];
+        save([cdirOut '/' cfile],'Iorig','Ibin','S');
+    end   % if isempty(Iorig) || isempty(S)
+    
+end  % for k = 1:numel(cfilenames)
+
