7e92a576ab5252380e5f2df0f4337af825afdb14
bgd88 <delbridge@berkeley.edu>
2018-10-13 11:00:13 -0400

    Example file for how to read the new mat files for training.

--- import_matfile.py
+++ import_matfile.py
@@ -1,19 +1,47 @@
 #!/usr/bin/env python3
-import scipy.io
+import h5py
 import glob
+import pandas as pd
 from myPath import dataDir
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Get the path to all .mat files
+filenames = glob.glob(dataDir + "images/*.mat")
 
-filenames = glob.glob(dataDir + "*.mat")
-
+# Lets do a simple Exmple with the first file in the List
 testFile = filenames[0]
-f = scipy.io.loadmat(testFile)
+f = h5py.File(testFile, 'r')
 
-# List all groups
-print("Keys: %s" % f.keys())
+# Cerate Pandas Dataframe
+df = pd.DataFrame()
 
-# Lets ge the S Structure
-S = f['S']
+# Convert from a view to a list to make it simplier/less abstract
+keys = list(f.keys())
+print("The keys are {}".format(keys))
 
-# Print fields
-print(S.dtype)
+# Loop over all values
+for key in f.keys():
+    print(key)
+    print(f[key].value)
+
+# Example of extracting variables - They are wrapped in lists for some reason
+imInt = f['imInt'].value.astype('int')[0]
+pixID = f['pixID'].value.astype('int')[0]
+nc = f['nc'].value.astype('int')[0][0]
+nr = f['nr'].value.astype('int')[0][0]
+
+plt.style.use('ggplot')
+
+# Note that you need to take care here, since Matlab writes arrays in a Fortran-like
+# sequencing whereas Pythin uses a C-like sequencing. They raster differently,
+# row then column versus column then row. i.e. Switch nc/nr or specifically tell it
+# to use a different ordering
+
+plt.imshow(np.reshape(imInt, [nr, nc], order="F"), cmap="bone")
+plt.title("Pixel Intensity Value", fontsize=20)
+plt.show()
+
+plt.imshow(np.reshape(pixID, [nr, nc], order="F"), cmap="rainbow")
+plt.title("Pixel Label", fontsize=20)
+plt.show()
