f3bbfea0d301d3eace5b41cb33761fa4841d4ece
Schmill731 <williamschmitt@college.harvard.edu>
2018-10-13 21:46:09 -0400

    modified neural net architecture to match input and output size (assuming classification problem, not regression); modified loss function to be categorical cross entropy

--- neural_net_training.py
+++ neural_net_training.py
@@ -11,47 +11,54 @@ print(tf.executing_eagerly())
 
 # Import Data
 from import_matfile import *
-train_data = ([f["imInt"].value, f["x"].value, f["y"].value], f["pixID"].value)
+train_data = {
+    "input": f["imInt"].value,
+    "parameters": [f["x"].value, f["y"].value], 
+    "output": f["pixID"].value}
 print(train_data)
 
-# # Define neural net architecture
-# class DigitSeisModel(tf.keras.Model):
-#     def __init__(self):
-#         super(DigitSeisModel, self).__init__()
-#         self.dense1 = tf.keras.layers.Dense(units=1000000)
+# Define neural net architecture
+class DigitSeisModel(tf.keras.Model):
+    def __init__(self):
+        super(DigitSeisModel, self).__init__()
+        self.input_layer = tf.keras.layers.Dense(units=2000000)
+        self.dense1 = tf.keras.layers.Dense(units=1000000)
+        self.output_layer = tf.keras.layers.Dense(units=8000000, activation=tf.nn.softmax)
 
-#         def call(self, input):
-#             """Runs the model."""
-#             result = self.dense1(input)
-#             return result
+        def call(self, input):
+            """Runs the model."""
+            result = self.input_layer(input)
+            result = self.dense1(result)
+            result = self.output_layer(result)
+            return result
 
-# # Create neural net
-# neuralNet = DigitSeisModel()
+# Create neural net
+neuralNet = DigitSeisModel()
 
-# # Define loss function
-# def loss(model, input, targets):
-#     prediction = model.call(input)
-#     return tf.losses.mean_squared_error(labels=targets, predictions=prediction)
+# Define loss function
+def loss(model, input, targets):
+    prediction = model.call(input)
+    return tf.losses.categorical_cross_entropy(y_true=targets, y_predict=prediction)
 
-# # Define function to measure gradient
-# def grad(model, inputs, targets):
-#     with tf.GradientTape() as tape:
-#         loss_value = loss(model, inputs, targets)
-#         return tape.gradient(loss_value, model.variables)
+# Define function to measure gradient
+def grad(model, inputs, targets):
+    with tf.GradientTape() as tape:
+        loss_value = loss(model, inputs, targets)
+        return tape.gradient(loss_value, model.variables)
 
-# # Define optimization function
-# optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+# Define optimization function
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
 
-# # Time to Train!
-# for (i, (x, y)) in enumerate(dataset_train):
-#     # Calculate derivates of the input function with respect to its parameters
-#     grads = grad(neuralNet, x, y)
-#     # Apply the gradient to the model
-#     optimizer.apply_gradients(zip(grads, neuralNet.variables),
-#         global_step=tf.train.get_or_create_global_step())
-#     print("Loss at step {:04d}: {:.3f}".format(i, loss(neural_net, x, y)))
+# Time to Train!
+for (i, (x, y)) in enumerate(dataset_train):
+    # Calculate derivates of the input function with respect to its parameters
+    grads = grad(neuralNet, x, y)
+    # Apply the gradient to the model
+    optimizer.apply_gradients(zip(grads, neuralNet.variables),
+        global_step=tf.train.get_or_create_global_step())
+    print("Loss at step {:04d}: {:.3f}".format(i, loss(neural_net, x, y)))
 
-# print("Final Loss: {:.3f}".format(loss(neuralNet, x, y)))
+print("Final Loss: {:.3f}".format(loss(neuralNet, x, y)))
 
 
 
