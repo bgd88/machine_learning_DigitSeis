def5d80e48a4094dfda3689c41136f7fe9e500a6
Schmill731 <williamschmitt@college.harvard.edu>
2018-11-01 20:24:10 -0400

    fixed accuracy report during training

--- cnn.py
+++ cnn.py
@@ -94,6 +94,12 @@ def cnn_model_fn(features, labels, mode):
 
   # Calculate Loss (for both TRAIN and EVAL modes)
   loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+  # Compute evaluation metrics
+  accuracy = tf.metrics.accuracy(labels=labels,
+                                 predictions=predictions["classes"],
+                                 name='acc_op')
+  metrics = {'accuracy': accuracy}
   tf.summary.scalar('accuracy', accuracy[1])
 
   # Configure the Training Op (for TRAIN mode)
@@ -105,11 +111,8 @@ def cnn_model_fn(features, labels, mode):
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
   # Add evaluation metrics (for EVAL mode)
-  eval_metric_ops = {
-      "accuracy": tf.metrics.accuracy(
-          labels=labels, predictions=predictions["classes"])}
   return tf.estimator.EstimatorSpec(
-      mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+      mode=mode, loss=loss, eval_metric_ops=metrics)
 
 # Reads an image from a file, decodes it into a dense tensor, and resizes it
 # to a fixed shape.
