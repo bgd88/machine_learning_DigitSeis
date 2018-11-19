5b626c02d09e5c478ae121f9581f35d682a643a3
bgd88 <bdelbridge@fas.harvard.edu>
2018-11-19 14:00:03 -0500

    Inital stab at getting the network to work on the full images

--- image_seg_scripts/image_gen_seis_data.py
+++ image_seg_scripts/image_gen_seis_data.py
@@ -22,65 +22,33 @@ author: bgd88
 from __future__ import print_function, division, absolute_import, unicode_literals
 
 import numpy as np
+import glob
+import h5py
+import matplotlib.pyplot as plt
 from tf_unet.image_util import BaseDataProvider
+from myPath import dataDir
 
-class GrayScaleDataProvider(BaseDataProvider):
+class DigitSeisDataProvider(BaseDataProvider):
     channels = 1
     n_class = 2
 
     def __init__(self, nx, ny, **kwargs):
-        super(GrayScaleDataProvider, self).__init__()
+        super(DigitSeisDataProvider, self).__init__()
+        self.filenames = glob.glob(dataDir + "images/*.mat")
         self.nx = nx
         self.ny = ny
         self.kwargs = kwargs
-        rect = kwargs.get("rectangles", False)
-        if rect:
-            self.n_class=3
 
     def _next_data(self):
-        return create_image_and_label(self.nx, self.ny, **self.kwargs)
+        fn = np.random.choice(self.filenames)
+        return create_image_and_label(fn , self.nx, self.ny, **self.kwargs)
 
-def create_image_and_label(nx,ny, cnt = 10, r_min = 5, r_max = 50,
-                           border = 92, sigma = 20, alpha=0.9, xWidth=3, yWidth=20):
+def create_image_and_label(filename, nx, ny):
 
-
-    image = np.ones((ny, nx, 1))
     label = np.zeros((ny, nx, 3), dtype=np.bool)
-    for _ in range(cnt):
-        a = np.random.randint(border, ny-border)
-        b = np.random.randint(border, nx-border)
-        r = np.random.randint(r_min, r_max)
-        h = np.random.randint(1,100)
+    image, mask = read_mat_file(filename)
+    label[mask>=0, 1] = 1
 
-        x,y = np.ogrid[-a:ny-a, -b:nx-b]
-        m = x*x + y*y <= r*r
-        # mask = np.logical_or(mask, m)
-        image[m] = h
-
-    yStart = np.random.randint(0, 400)
-    delY = np.random.randint(290, 310)
-    h2 = np.random.randint(250,255)
-    mask = np.zeros((ny, nx))
-    for tNum in range(3):
-        y = yWidth*generate_ts(nx, alpha) + yStart + tNum*delY
-        for ii in np.arange(nx):
-        #     for yOff in np.arange(-yWidth, yWidth+1):
-        #         for xOff in np.arange(-xWidth, xWidth+1):
-                    # xInd = ii + xOff
-                    # pInd = y[ii] + yOff
-                    xMin = max([0, ii-xWidth])
-                    xMax = min([nx, ii+xWidth])
-                    yMin = max([0, y[ii]-yWidth])
-                    yMax = min([ny, y[ii]+yWidth])
-                    try:
-                        mask[yMin:yMax, xMin:xMax] = 1
-                        image[yMin:yMax, xMin:xMax] = h2
-                    except:
-                        continue
-
-    label[pixID>=0, 1] = 1
-
-    image += np.random.normal(scale=sigma, size=image.shape)
     image -= np.amin(image)
     image /= np.amax(image)
     return image, label[..., 1]
@@ -96,19 +64,21 @@ def read_mat_file(filename):
     image = np.ones((nr, nc, 1))
     label = np.zeros((nr, nc, 3), dtype=np.bool)
 
-    image = np.reshape(imInt, [nr, nc, 1], order="F")
-    pixID = np.reshape(pixID, [nr, nc], order="F")
-plt.imshow(, cmap="bone")
-plt.title("Pixel Intensity Value", fontsize=20)
-plt.show()
-
-plt.imshow(, cmap="rainbow")
+    image = np.reshape(imInt, [nr, nc, 1], order="F").astype(float)
+    mask = np.reshape(pixID, [nr, nc], order="F")
+    f.close()
+    return image, mask
 
 def plot_image_label(image, label):
     fig, ax = plt.subplots(1,2, figsize=[15,5])
-    ax[0].imshow(image[:, :, 0], cmap='bone')
+    ax[0].imshow(image[0,:,:, 0], cmap='bone')
     ax[0].set_title('Test Image', fontsize=20)
-    ax[1].imshow(label[:, :, 1])
+    ax[1].imshow(label[0,:, :, 1])
     ax[1].set_title('True Label', fontsize=20)
     plt.savefig('Test_Image_Labels.pdf', dpi=100, fmt='pdf')
     plt.close()
+
+if __name__ == '__main__':
+    generator = DigitSeisDataProvider(2000, 1000)
+    image, label = generator(1)
+    plot_image_label(image, label)
---
+++ image_seg_scripts/train_digitSeis_cnn.py
@@ -0,0 +1,47 @@
+from __future__ import division, print_function
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import image_gen_seis_data
+from tf_unet import unet
+from tf_unet import util
+
+plt.rcParams['image.cmap'] = 'bone'
+nx = 2000
+ny = 1000
+
+generator = image_gen_seis.DigitSeisDataProvider(nx, ny)
+x_test, y_test = generator(1)
+fig, ax = plt.subplots(1,2, sharey=True, figsize=(8,4))
+ax[0].imshow(x_test[0,...,0], aspect="auto")
+ax[1].imshow(y_test[0,...,1], aspect="auto")
+plt.show()
+
+
+net = unet.Unet(channels=generator.channels, n_class=generator.n_class,
+                layers=3, features_root=64)
+
+trainer = unet.Trainer(net, optimizer="momentum", opt_kwargs=dict(momentum=0.2))
+path = trainer.train(generator, "./unet_trained", training_iters=32, epochs=10,
+                        display_step=2)
+
+
+x_test, y_test = generator(1)
+prediction = net.predict("./unet_trained/model.ckpt", x_test)
+
+fig, ax = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(12,5))
+ax[0].imshow(x_test[0,...,0], aspect="auto")
+ax[1].imshow(y_test[0,...,1], aspect="auto")
+mask = prediction[0,...,1] > 0.4
+ax[2].imshow(mask, aspect="auto")
+ax[0].set_title("Input")
+ax[1].set_title("Ground truth")
+ax[2].set_title("Prediction")
+fig.tight_layout()
+plt.savefig('Test_results.png')
+plt.close()
+
+plt.imshow(prediction[0,:,:,1])
+plt.colorbar()
+plt.savefig('Prediction_results.png')
+plt.close()
